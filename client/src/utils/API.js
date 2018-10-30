94e62c8d903c7f51571b8b96ff3917da13e63739
Poltergeist <brandonssc@gmail.com>
2018-10-29 23:40:35 -0400

    mixed content fix maybe

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const url = "http://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=" + "b9f91d369ff59547cd47b931d8cbc56b:0:74623931";
+const url = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=" + "b9f91d369ff59547cd47b931d8cbc56b:0:74623931";
 
 export default {
   // Gets all articles
